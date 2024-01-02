7a95b9508d5dc5dc3759719f49792378e295cf6c
Jackson Silva <cajui54@gmail.com>
2024-01-02 10:33:48 -0300

    fixing bugs

--- src/hooks/useRequest.js
+++ src/hooks/useRequest.js
@@ -67,7 +67,7 @@ const useAxios = ({url, config = {}}) => {
             
         }
         
-        requestDatas(callFetch);
+        requestDatas();
 
         return () => {
             controller.abort();
